416936a4c2a15fa7faaf9aa3ac359b26fd7627f4
sandip gawali <gawalisandip150@gmail.com>
2025-06-24 12:42:46 +0530

    video uploader task completed

--- video-uploader/frontend/src/LogoutButton.tsx
+++ video-uploader/frontend/src/LogoutButton.tsx
@@ -1,36 +1,3 @@
-// import { googleLogout } from "@react-oauth/google";
-
-// interface LogoutButtonProps {
-//   onLogout: () => void;
-// }
-
-// const LogoutButton = ({ onLogout }: LogoutButtonProps) => {
-//   const handleLogout = () => {
-//     googleLogout(); // End session
-//     localStorage.removeItem("googleUser"); // Remove stored user
-//     onLogout(); // Notify parent to update UI
-//   };
-
-//   return (
-//     <button
-//       onClick={handleLogout}
-//       style={{
-//         padding: "10px 20px",
-//         backgroundColor: "#db4437",
-//         color: "#fff",
-//         border: "none",
-//         borderRadius: "5px",
-//         cursor: "pointer",
-//       }}
-//     >
-//       Logout
-//     </button>
-//   );
-// };
-
-// export default LogoutButton;
-
-
 import { googleLogout } from "@react-oauth/google";
 
 interface LogoutButtonProps {
